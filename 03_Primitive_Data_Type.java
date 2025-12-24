e25d64c2c472833c7617f287051fa3c07e1fa370
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-24 22:37:17 +0530

    03_Primitive_Data_Type.java

--- 03_Primitive_Data_Type.java
+++ 03_Primitive_Data_Type.java
@@ -1,1 +1,16 @@
+public class primitive_data_type {
+    public static void main(String[] args) {
+
+        int age = 20;
+        int marks = 85;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+        System.out.println(isPassed);
+    }
+    
+}
 
