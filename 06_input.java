d94a589439069cd712420bf3ab0cd9950e857ec0
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-28 23:52:57 +0530

    06_input.java

--- 06_input.java
+++ 06_input.java
@@ -1,1 +1,12 @@
-
+//For asking input from user we use scanner object 
+//Scanner sc=new Scanner(System.in);
+//Example
+import java.util.*;
+class input{
+  public static void main(String[] args){
+    //Input
+  Scanner sc = new Scanner(System.in);
+    String name=sc.nextLine();
+   System.out.println(name);
+  }
+}
