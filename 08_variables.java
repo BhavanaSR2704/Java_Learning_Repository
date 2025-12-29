5fd4d3ec98bfa6d807e86ceb7ba00e6849e32bec
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-29 23:09:44 +0530

    08_variables.java

--- 08_variables.java
+++ 08_variables.java
@@ -1,1 +1,11 @@
 
+public class variables {
+     public static void main(String[] args){
+        //Variables
+        int a=10;
+        int b=25;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+    
+}
