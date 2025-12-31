45f6556b3b62c882ab4aaa6318bb375b94fda29a
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-31 23:02:17 +0530

    12_for_loop.java

--- 12_for_loop.java
+++ 12_for_loop.java
@@ -2,3 +2,26 @@
 // for(initialisation;condition;updation){
 //     //do someting
 // }
+
+
+public class for_loop {
+    public static void main(String[] args) {
+        for(int i=0;i<=6;i++){
+            System.out.println("hello guys");
+        }
+    }
+    
+}
+
+
+
+//=============================================================
+//Printing 1-10 numbers
+
+public class for_loop {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.println(i);//if we use print(i+" ") it will print in same line
+        }
+    }
+}
