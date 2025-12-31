7ba5c56ac96d13f4ca171ef0af3c7c9e32fc90d6
BhavanaSR2704 <bhavanasr272006@gmail.com>
2025-12-31 23:04:44 +0530

    13_while_loop.java

---
+++ 13_while_loop.java
@@ -0,0 +1,17 @@
+//SYNTAX===============>
+    // while(condition){
+    //     //do something
+    // }
+
+
+public class while_loop {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+    
+}
