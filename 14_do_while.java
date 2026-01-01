87241a4c7a1d7ef21b29e073ba85e26772792ede
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-01 18:58:57 +0530

    14_do_while.java

--- 14_do_while.java
+++ 14_do_while.java
@@ -1,1 +1,18 @@
+// SYNTAX==================>
+//     do{
+//         //Do something
+//     }while(condition);//NOTE ";" has to be written in last of while 
+
+
+
+public class do_while {
+    public static void main(String[] args) {
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<11);
+    }
+    
+}
 
