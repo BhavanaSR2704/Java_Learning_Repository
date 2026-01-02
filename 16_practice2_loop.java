c93cdc29087b11aef11b50a369493164f66acc05
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-02 21:11:40 +0530

    16_practice2_loop.java

--- 16_practice2_loop.java
+++ 16_practice2_loop.java
@@ -1,1 +1,17 @@
 
+// Printing the sum of first N natural numbers
+
+
+import java.util.*;
+public class practice_loop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number n:");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum=sum+i;
+        }
+            System.out.println(sum);
+    }
+}
