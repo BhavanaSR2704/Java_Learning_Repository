6ff528d574327ee776f7ec5fa8ddcdf8ec08b8db
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-13 20:40:45 +0530

    25_pattern_07_Inverted_Half_pyramid_with_number.java

---
+++ 25_pattern_07_Inverted_Half_pyramid_with_number.java
@@ -0,0 +1,21 @@
+
+public class Inverted_Half_pyramid_with_number {
+     public static void main(String[] args) {
+        int n=5;
+        //outer loop
+        for(int i=n;i>=1;i--){
+            //inner loop
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//======================O/P:-
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
