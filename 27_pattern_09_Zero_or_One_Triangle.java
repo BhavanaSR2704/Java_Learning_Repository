6bbb0721a5f213d1d781ea479a290a6fb15b020e
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-14 22:54:09 +0530

    27_pattern_09_Zero_or_One_Triangle.java

--- 27_pattern_09_Zero_or_One_Triangle.java
+++ 27_pattern_09_Zero_or_One_Triangle.java
@@ -1,1 +1,27 @@
 
+public class Zero_or_One_Triangle {
+    public static void main(String[] args) {
+        int n=5;
+        //outer loop
+        for(int i=1;i<=n;i++){
+            //inner loop
+            for(int j=1;j<=i;j++){
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print(" 1 ");
+                }
+                else{
+                    System.out.print(" 0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+//=============================================>O/P:-
+ 1 
+ 0  1 
+ 1  0  1 
+ 0  1  0  1 
+ 1  0  1  0  1 
