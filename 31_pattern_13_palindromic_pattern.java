f07c1e5ea20f8aae72ccdd962af2e9c376702d6a
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-19 23:08:12 +0530

    31_pattern_13_palindromic_pattern.java

--- 31_pattern_13_palindromic_pattern.java
+++ 31_pattern_13_palindromic_pattern.java
@@ -1,1 +1,30 @@
 
+
+public class palindromic_pattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //1st half numbers
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            //2nd half numbers
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+//===================================================>O/P:-
+    1
+   212
+  32123
+ 4321234
+543212345
