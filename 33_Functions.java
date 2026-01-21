955286170ec55e49173b7481c5937c6a98ca59f2
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-21 23:59:45 +0530

    33_Functions.java

--- 33_Functions.java
+++ 33_Functions.java
@@ -1,1 +1,9 @@
 
+//SYNTAX:-
+returnType functionName(type arg1,type args2){
+  //operations
+}
+
+
+//==========================================================================
+//Printing a name by using function
