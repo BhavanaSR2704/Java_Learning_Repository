855e9c6aeffce25ad48c85486c27099bcb8b6d78
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-23 21:49:56 +0530

    35_Functions_Multiply_02.java

--- 35_Functions_Multiply_02.java
+++ 35_Functions_Multiply_02.java
@@ -1,1 +1,19 @@
 
+import java.util.*;
+
+public class Functions_Multiply {
+     public static int calculatemul(int a,int b){
+    return a*b;
+ }   
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println("Multiply of a*b is:"+calculatemul(a,b));
+ } 
+}
+
+//=======================>O/P:-
+8
+9
+Multiply of a*b is:72
