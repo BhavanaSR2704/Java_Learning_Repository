06ff2dd16191562886a77c1857e3392ec6915152
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-01-26 23:30:49 +0530

    37_Functions_CalculateAvg_04.java

--- 37_Functions_CalculateAvg_04.java
+++ 37_Functions_CalculateAvg_04.java
@@ -1,2 +1,24 @@
 
 
+import java.util.*;
+public class Function_CalculateAvg {
+    public static int CalculateAvg(int a, int b,int c){
+        int avg=(a+b+c)/3;
+        return avg;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(); 
+        int b=sc.nextInt(); 
+        int c=sc.nextInt();
+        int avg=CalculateAvg(a,b,c);
+        System.out.println("Avg of 3 numbers is:"+avg);
+    }
+}
+
+
+//=======================================>O/P:-
+8
+7
+3
+Avg of 3 numbers is:6
