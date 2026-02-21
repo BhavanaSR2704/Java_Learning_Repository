1399d4f04d2f859df7f2385948598d1cce61c62a
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-02-21 23:32:26 +0530

    48_Defined_Array.java

---
+++ 48_Defined_Array.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+        int marks[]={98,99,97};
+        // marks[0]=97;
+        // marks[1]=98;
+        // marks[2]=99;
+        //System.out.println(marks[0]);
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
+
+//====================================================>O/P:-
+98
+99
+97
