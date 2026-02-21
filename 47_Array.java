a1b3224cdd6820e88bcede8f8ae571acc22d4161
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-02-21 23:25:42 +0530

    47_Array.java

---
+++ 47_Array.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+        int marks[]=new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=99;
+        //System.out.println(marks[0]);
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
+//=================================================O/P:-
+97
+98
+99
+
+
