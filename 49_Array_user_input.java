8d0ddae22baafb3285d5d396acf0a3b26f2618b3
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-02-22 23:09:34 +0530

    49_Array_user_input.java

---
+++ 49_Array_user_input.java
@@ -0,0 +1,20 @@
+//Taking an array as input from the user.Search for a given number X and printing the index at which it occurs.
+
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int size=sc.nextInt();
+       int nums[]=new int[size];
+       for(int i=0;i<size;i++){
+        nums[i]=sc.nextInt();
+       }
+       int x=sc.nextInt();
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==x){
+            System.out.println("X found at index: "+i);
+        }
+       }
+    }
+}
+
