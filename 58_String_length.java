621a56b787643e97c8b30bcdb6b3ec61554fd6f9
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-03-20 13:01:50 +0530

    58_String_length.java

---
+++ 58_String_length.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class String_Concatination {
+    public static void main(String[] args) {
+        String firstname="Bhavana";
+        String lastname="S R";
+        String Fullname=firstname+" "+lastname;
+        System.out.println(Fullname.length());
+    }
+}
