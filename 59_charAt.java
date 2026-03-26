16f6c2b71f40be3107c8c1f4c83e766f159f9036
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-03-26 23:59:07 +0530

    59_charAt.java

--- 59_charAt.java
+++ 59_charAt.java
@@ -1,5 +1,13 @@
 
 
+public class CharAtLoop {
+    public static void main(String[] args) {
+        String name = "Bobby";
 
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+    }
+}
 
 
