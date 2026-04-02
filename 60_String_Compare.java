097b18c259109c74e99a99bcc14029eb87d333ae
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-04-02 23:34:37 +0530

    60_String_Compare.java

--- 60_String_Compare.java
+++ 60_String_Compare.java
@@ -1,3 +1,13 @@
 
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = "hello";
+
+        System.out.println(s1.equals(s2)); // true
+        System.out.println(s1.equals(s3)); // false (case-sensitive)
+    }
+}
 
 
