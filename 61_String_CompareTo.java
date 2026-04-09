9e04feb1c06825e9f2f3d42f67afcf298f35c0be
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-04-09 23:33:58 +0530

    61_String_CompareTo.java

---
+++ 61_String_CompareTo.java
@@ -0,0 +1,14 @@
+
+public class StringCompareTo {
+    public static void main(String[] args) {
+        String s1 = "Apple";
+        String s2 = "Banana";
+
+        if(s1.compareTo(s2)==0){
+            System.out.println("String are equal");     
+        }
+        else{
+            System.out.println("String are not equal");
+        }
+    }
+}
