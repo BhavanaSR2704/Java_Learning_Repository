04a088ded8d734eadcd75769bc0014796f919455
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-04-10 23:30:09 +0530

    62_Sub_String.java

---
+++ 62_Sub_String.java
@@ -0,0 +1,8 @@
+public class sub_String {
+    public static void main(String[] args) {
+        String sentance="I am Bhavana";
+        //Substring (Beg index,end index)
+        String name =sentance.substring(4,sentance.length());
+        System.out.println(name);
+    }
+}
