bc39419250b3007e62733ed43c99b0414ce79352
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-04-13 23:31:36 +0530

    64_String_Builder_inserting.java

---
+++ 64_String_Builder_inserting.java
@@ -0,0 +1,9 @@
+
+public class String_Builder {
+    public static void main(String[] args) {
+         StringBuilder sb=new StringBuilder("Tony");
+         System.out.println(sb);
+         sb.insert(2,'n');
+         System.out.println(sb);
+    }
+}
