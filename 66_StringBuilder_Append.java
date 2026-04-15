fc2fae9bfa9863f0f05a1007b25a488c63800255
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-04-15 20:24:12 +0530

    66_StringBuilder_Append.java

---
+++ 66_StringBuilder_Append.java
@@ -0,0 +1,12 @@
+ 
+
+public class String_Builder {
+    public static void main(String[] args) {
+         StringBuilder sb=new StringBuilder("H");
+         sb.append("e");//str=str="e";
+         sb.append("l");
+         sb.append("l");
+         sb.append("o"); 
+         System.out.println(sb);
+    }
+}
