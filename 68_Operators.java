659a29e459bba6eb5130af06c3d58ca677b4ab3b
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-04-17 23:45:21 +0530

    68_Operators.java

---
+++ 68_Operators.java
@@ -0,0 +1,26 @@
+Symbols that tells compailer to perform some operation 
+*Binary[=,-,*,/,%]
+*Unary[++,--]
+  Unary
+  ** PreIncrement
+  =>++a
+  1.Change value
+  2.use value
+  Example:-
+    int a=10;
+    int b=0;
+    b=++a;  //b=11
+    System.out.println(a);  //a=11
+    System.out.println(b);  //b=11
+
+
+  **PostIncrement
+  =>a++
+  1.use value
+  2.change value
+  Example:-
+    int a=10;
+    int b=0;
+    b=a++;  //b=10
+    System.out.println(a);  //a=11
+    System.out.println(b);  //b=10
