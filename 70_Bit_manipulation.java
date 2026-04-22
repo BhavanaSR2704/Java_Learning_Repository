155767ed0ee1b9b8f2121742a8a3f44883c5af26
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-04-22 22:16:02 +0530

    70_Bit_manipulation.java

---
+++ 70_Bit_manipulation.java
@@ -0,0 +1,28 @@
+1.Get bit operation
+2.Set bit operation
+3.Clear bit operation
+4.Update bit operation
+
+
+
+
+
+1.Get bit operation
+  Bit mask:1<<i(i=position)
+  Operation :AND
+
+  Code:
+      import java.util.*;
+public class Bitmanipulation1 {
+        public static void main(String[] args) {
+        int n=5;
+        int ops=2;
+        int bitmask=1<<pos;
+    if((bitmask & n)==0);
+        System.out.println("Bit is zero");
+    }
+    else{
+        System.out.println("Bit is one");
+    }
+}
+
