1c9dcb4af8ddd983c7f3c0783544312a96dd911e
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-05-22 23:28:05 +0530

    83_Recursion_07.java

--- 83_Recursion_07.java
+++ 83_Recursion_07.java
@@ -1,2 +1,27 @@
 
 
+//Print X^n(Stack height=long)
+
+
+
+public class Recursion_7 {
+    public static int  calpower(int x,int n){
+        if(n==0){ //base case 1
+            return 1;
+        }
+        if(x==0){ //base case 2
+            return 0;
+        }
+       if(n%2==0){
+        return calpower(x, n/2) * calpower(x,n/2);
+       }
+       else{
+        return calpower(x, n/2) * calpower(x,n/2)*x;
+       }
+    }
+    public static void main(String[] args) {
+        int x=2,n=5;
+        int ans=calpower(x,n);
+        System.out.println(ans);
+    }
+}
