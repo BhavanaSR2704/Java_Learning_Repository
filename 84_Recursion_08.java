6d71b6505b885f2b4eed10a0a5ec3babd68f9d58
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-05-28 13:15:32 +0530

    84_Recursion_08.java
    Implemented a recursive solution for the Tower of Hanoi problem, allowing for the transfer of disks between pegs.

--- 84_Recursion_08.java
+++ 84_Recursion_08.java
@@ -1,2 +1,23 @@
 
+//Solving the Tower of Hanoi problem using recursion
+
+public class Recursion_08 {
+    public static void towerOfHanoi(int n, char source, char helper, char destination){
+        if(n==1){ //base case
+            System.out.println("Transfer disk "+n+" from "+source+" to "+destination);
+            return;
+        }
+        //recursive call 1: transfer n-1 disks from source to helper using destination as helper
+        towerOfHanoi(n-1, source, destination, helper);
+        //transfer the nth disk from source to destination
+        System.out.println("Transfer disk "+n+" from "+source+" to "+destination);
+        //recursive call 2: transfer n-1 disks from helper to destination using source as helper
+        towerOfHanoi(n-1, helper, source, destination);
+    }
+    public static void main(String[] args) {
+        int n=3; //number of disks
+        towerOfHanoi(n, 'A', 'B', 'C'); //A is source, B is helper, C is destination
+    }
+}
+
 
