996ab8dae27d6f3e8cf15d9920d34d8f878e0ca3
BhavanaSR2704 <bhavanasr272006@gmail.com>
2026-07-09 21:43:43 +0530

    95_Recursion_19.java
    Implement a method to print all permutations of a string using recursion.

---
+++ 95_Recursion_19.java
@@ -0,0 +1,28 @@
+
+//print all permutations of a string
+public class Recursion_19 {
+    public static void main(String[] args) {
+        String str = "ABC";
+        permute(str, 0, str.length() - 1);
+    }
+
+    public static void permute(String str, int left, int right) {
+        if (left == right) {
+            System.out.println(str);
+        } else {
+            for (int i = left; i <= right; i++) {
+                str = swap(str, left, i);
+                permute(str, left + 1, right);
+                str = swap(str, left, i); // backtrack
+            }
+        }
+    }
+
+    public static String swap(String str, int i, int j) {
+        char[] arr = str.toCharArray();
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return new String(arr);
+    }
+}
